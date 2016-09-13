967d574e5d8a5cdf1ccba1410531e34f06d467e5
akosborn <andrewosborn93@gmail.com>
2016-09-13 02:51:07 -0500

    Removed variable declarations and localized others

--- Account.java
+++ Account.java
@@ -11,28 +11,30 @@ import java.util.Scanner;
 
 public class Account implements Serializable
 {
-    protected Scanner input = new Scanner(System.in);
-    private int accountNumber, pin, entryInt;
-    protected String entryString;
-    private String nameFirst, nameLast, fullName;
+    Scanner input = new Scanner(System.in);
+    private int accountNumber, pin;
+    private String nameFirst, fullName;
     private Random random = new Random();
-    protected final static double initialBalance = 0.00;
-    protected double checkingBalance = initialBalance, savingsBalance = initialBalance;
-    public static ArrayList<Account> accountNumbers = new ArrayList<Account>();
+    final static double INITIAL_BALANCE = 0;
+    static ArrayList<Account> accountNumbers = new ArrayList<Account>();
 
-    public void createAccount() // sets acctNumber and pin and adds account to ArrayList<Account> accountNumbers
+    public Account() // sets acctNumber and pin and adds account to ArrayList<Account> accountNumbers
     {
         System.out.println("\nTo create your account, you must complete the following fields.");
         setName();
         setNumAndPin();
         accountNumbers.add(this);
         System.out.println(accountNumbers);
+
         System.out.println("\nThank you, " + nameFirst + ". Your account (#" + this.accountNumber + ") has been created.\nYou " +
                 "will now be required to sign in with your new login information.\n");
+        validateLogin();
     }
 
     private void setName() // associates first and last name with new user's account
     {
+        String nameLast;
+
         System.out.print("First name: \n> ");
         nameFirst = input.next();
 
@@ -56,8 +58,10 @@ public class Account implements Serializable
         pin = input.nextInt();
     }
 
-    public Account validateLogin() // uses do-while loops to verify submitted account and pin
+    static Account validateLogin() // uses do-while loops to verify submitted account and pin
     {
+        Scanner input = new Scanner(System.in);
+        int entryInt;
         boolean acctFound = false;
         boolean pinMatches = false;
         Account returnAccount = null;
