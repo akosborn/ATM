b31762e1d7eab8644988a0f0663deac1a93cf635
akosborn <andrewosborn93@gmail.com>
2016-09-14 13:29:34 -0500

    Added withdraw()

--- CheckingAccount.java
+++ CheckingAccount.java
@@ -23,4 +23,20 @@ public class CheckingAccount extends Account
         checkingBalance += depositAmount;
         System.out.println("Your new checking balance is $" + checkingBalance + ".\n");
     }
+
+    @Override
+    public void withdraw()
+    {
+        double withdrawAmount = 0;
+
+        while (withdrawAmount != 20 && withdrawAmount != 40 && withdrawAmount != 60 && withdrawAmount != 80 && withdrawAmount != 100)
+        {
+            System.out.print("Choose an amount to withdraw.\n\t$20\n\t$40\n\t$60\n\t$80\n\t$100\n>$");
+            withdrawAmount = input.nextInt();
+            System.out.println("");
+        }
+
+        checkingBalance -= withdrawAmount;
+        System.out.println("Your new checking balance is $" + checkingBalance + ".\n");
+    }
 }
