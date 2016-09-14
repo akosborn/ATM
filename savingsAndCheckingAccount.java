4d5a989fac78f601d4a65f116fb05fe94ea38ecb
akosborn <andrewosborn93@gmail.com>
2016-09-14 13:06:28 -0500

    Added withdraw()

--- savingsAndCheckingAccount.java
+++ savingsAndCheckingAccount.java
@@ -71,4 +71,43 @@ public class savingsAndCheckingAccount extends Account
         System.out.println("Transfer complete.");
         getBalance();
     }
+
+    @Override
+    public void withdraw()
+    {
+        String entryString;
+        double withdrawAmount = 0;
+
+        System.out.print("Withdraw from checking (c) or savings (s)?\n> ");
+        entryString = input.next();
+        while (!entryString.equals("c") && !entryString.equals("s"))
+        {
+            System.out.println("Invalid entry. Enter \"c\" to deposit to checking or \"s\" to deposit to savings");
+            entryString = input.next();
+        }
+
+        if (entryString.equals("c")) // if user chooses to deposit to checking
+        {
+            while (withdrawAmount != 20 && withdrawAmount != 40 && withdrawAmount != 60 && withdrawAmount != 80 && withdrawAmount != 100)
+            {
+                System.out.print("Choose an amount to withdraw.\n\t$20\n\t$40\n\t$60\n\t$80\n\t$100\n>$");
+                withdrawAmount = input.nextInt();
+            }
+
+            checkingBalance -= withdrawAmount;
+            System.out.println("Your new checking balance is $" + checkingBalance + ".\n");
+
+        } else if (entryString.equals("s")) // if user chooses to deposit to savings
+        {
+            while (withdrawAmount != 20 && withdrawAmount != 40 && withdrawAmount != 60 && withdrawAmount != 80 && withdrawAmount != 100)
+            {
+                System.out.print("Choose an amount to withdraw.\n\t$20\n\t$40\n\t$60\n\t$80\n\t$100\n>$");
+                withdrawAmount = input.nextInt();
+            }
+
+            savingsBalance -= withdrawAmount;
+            System.out.println("Your new savings balance is $" + savingsBalance + ".\n");
+        }
+    }
 }
+
